466860e468a43b5a46bb003ac6a02946f48ffe2e
bebob2 <konto2228@gmail.com>
2023-01-29 16:54:51 +0100

    inirtial release

---
+++ src/app.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+  const handleChange = (event) => {
+    console.log("bia", event.target);
+    setNewName(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (newName.trim() === "") {
+      return;
+    }
+    setPersons([...persons, { name: newName }]);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input onChange={handleChange} value={newName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+
+      <ul>
+        {persons.map((person, index) => {
+          const { name } = person;
+          return <li key={index}>{name}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
