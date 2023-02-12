6656b9dc9572273f500701110bd26e00aa94e2b9
bebob2 <konto2228@gmail.com>
2023-02-12 17:28:40 +0100

    added json-server

--- src/app.jsx
+++ src/app.jsx
@@ -1,18 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Field } from "./components/field";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
 
   const [formState, setFormState] = useState({
     name: "bia",
     number: "01794334642",
   });
+  useEffect(() => {
+    const getPersons = async () => {
+      try {
+        const response = await axios.get("/api/persons");
+        const { data } = response;
+        if (!data) {
+          throw new Error("no data");
+        }
+        setPersons(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getPersons();
+  }, []);
 
   const { name: newName, number: newNumber } = formState;
 
